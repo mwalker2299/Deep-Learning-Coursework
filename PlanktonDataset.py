dbdf9be007228c86dea1c34056444a468a3b504d
Matthew Walker <2310528w@student.gla.ac.uk>
2022-03-24 16:55:12 +0000

    Inital Dataset file

---
+++ PlanktonDataset.py
@@ -0,0 +1,24 @@
+from torch.utils.data import Dataset
+
+class PlanktonDataset(Dataset):
+    def __init__(self, plankton_dict, transform, target_transform):
+        self.labels           = plankton_dict['labels']
+        self.images           = plankton_dict['images']
+        self.image_names      = dict((v,k) for k,v in plankton_dict['names'].items())
+        self.transform        = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+    def getimagename(self, label):
+      return self.image_names[label]
